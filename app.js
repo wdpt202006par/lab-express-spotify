7dc7836c3b6e5ffae0a4d952e373d947e7008e15
psophielam <psophielam92@gmail.com>
2020-08-29 15:46:39 +0200

    iteration 1 almost done

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const hbs = require('hbs');
 
 // require spotify-web-api-node package here:
+const SpotifyWebApi = require('spotify-web-api-node');
 
 const app = express();
 
@@ -12,7 +13,19 @@ app.set('views', __dirname + '/views');
 app.use(express.static(__dirname + '/public'));
 
 // setting the spotify-api goes here:
-
+const spotifyApi = new SpotifyWebApi({
+    clientId: '630dac8e754e4304a226f98566ecc8fc',
+    clientSecret: '1992731b3027466daca8546bca3c8d6f'
+  });
+  
+  // Retrieve an access token
+  spotifyApi
+  .clientCredentialsGrant()
+  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
+  .catch(error => console.log('Something went wrong when retrieving an access token', error));
+ 
+  
 // Our routes go here:
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+
