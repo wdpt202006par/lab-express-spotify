eaf908b5bf3a57b6394aecc7edb2767809fa4334
Nina Quaresma <quaresma.nina@gmail.com>
2020-08-29 16:04:11 +0200

    Iteration 3

--- app.js
+++ app.js
@@ -16,14 +16,22 @@ app.use(express.static(__dirname + '/public'));
 const spotifyApi = new SpotifyWebApi({
     clientId: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET
-  });
-  
-  // Retrieve an access token
-  spotifyApi
+});
+
+// Retrieve an access token
+spotifyApi
     .clientCredentialsGrant()
     .then(data => spotifyApi.setAccessToken(data.body['access_token']))
     .catch(error => console.log('Something went wrong when retrieving an access token', error));
 
 // Our routes go here:
+app.get('/home', function (req, res, next) {
+    res.render('index');
+})
+
+app.get('/artist-search', function (req, res, next) {
+    const artist = req.query
+    console.log(artist)
+})
 
 app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
