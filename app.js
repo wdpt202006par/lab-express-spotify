0db9082d8926467de707b466aebaed1e0bcff591
James <jamescarclan@yahoo.com>
2020-08-29 16:57:10 +0200

    done2

--- app.js
+++ app.js
@@ -1,36 +1,55 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express');
-const hbs = require('hbs');
+const express = require("express");
+const hbs = require("hbs");
 
 // require spotify-web-api-node package here:
 
-var SpotifyWebApi = require('spotify-web-api-node');
- 
+var SpotifyWebApi = require("spotify-web-api-node");
+const app = express();
+
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.use(express.static(__dirname + "/public"));
+
 // credentials are optional
-var spotifyApi = new SpotifyWebApi({
-  clientId: process.env.clientId,
-  clientSecret: process.env.clientSecret,
-  redirectUri: 'http://www.example.com/callback'
+const spotifyApi = new SpotifyWebApi({
+  clientId: process.env.CLIENT_ID,
+  clientSecret: process.env.CLIENT_SECRET,
 });
 
 // Retrieve an access token
 spotifyApi
   .clientCredentialsGrant()
-  .then(data => spotifyApi.setAccessToken(data.body['access_token']))
-  .catch(error => console.log('Something went wrong when retrieving an access token', error));
-
-const app = express();
-
-app.set('view engine', 'hbs');
-app.set('views', __dirname + '/views');
-app.use(express.static(__dirname + '/public'));
+  .then((data) => spotifyApi.setAccessToken(data.body["access_token"]))
+  .catch((error) =>
+    console.log("Something went wrong when retrieving an access token", error)
+  );
 
 // setting the spotify-api goes here:
 
 // Our routes go here:
 
-app.get('/', (req,res,next) => {
-  res.render("home")
-})
-app.listen(3000, () => console.log('My Spotify project running on port 3000 🎧 🥁 🎸 🔊'));
+app.get("/", (req, res, next) => {
+  res.render("home");
+});
+
+app.get("/artist-search", (req, res, next) => {
+  spotifyApi
+    .searchArtists(req.query.artist)
+    .then((data) => {
+      console.log("Les données reçues de l'API:", data.body);
+      // ----> 'ICI CE QUE NOUS VOULONS FAIRE APRÈS RECEVOIR LES DONNÉES DE L'API'
+      res.render("artist-search");
+    })
+    .catch((err) =>
+      console.log(
+        "L'erreur lors de la recherche d'artistes s'est produite:",
+        err
+      )
+    );
+});
+
+app.listen(3000, () =>
+  console.log("My Spotify project running on port 3000 🎧 🥁 🎸 🔊")
+);
